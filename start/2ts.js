98882382ffd953925222fde055149ed2a3b250ce
svarunk <162920915+svarunk@users.noreply.github.com>
2024-12-11 17:09:15 +0000

    type convertion

---
+++ start/2ts.js
@@ -0,0 +1,14 @@
+/*let num="33";
+console.log(typeof(num))
+let int=Number(num)
+console.log(typeof(int));
+let i = 0
+console.log(typeof(i))
+let bol=Boolean(i);
+console.log(typeof(bol));
+*/
+let i=56
+console.log(typeof(i))
+let st = String(i);
+console.log(typeof(st));
+console.log(i);
